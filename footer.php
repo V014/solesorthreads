61f0ed17bb3c6e996343bf43bdc92178a8431a3f
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-29 09:21:00 +0200

    update footer.php and index.php

--- footer.php
+++ footer.php
@@ -55,3 +55,10 @@
     </div>
     </div>
 </div>
+<!-- copyright section start -->
+<div class="copyright_section">
+    <div class="container">
+    <p class="copyright_text">2020 All Rights Reserved. Design by <a href="https://html.design">Free html  Templates</a></p>
+    </div>
+</div>
+<!-- copyright section end -->
